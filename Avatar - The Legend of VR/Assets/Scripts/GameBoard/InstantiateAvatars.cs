4e30a4c3f6ff8282172015b2dfb85d2ff66fa136
Michael Hüppe <mhueppe@uos.de>
2022-02-05 14:02:36 +0100

    made avatar Prefab and refactored materials

--- Avatar - The Legend of VR/Assets/Scripts/GameBoard/InstantiateAvatars.cs
+++ Avatar - The Legend of VR/Assets/Scripts/GameBoard/InstantiateAvatars.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class InstantiateAvatars : MonoBehaviour
@@ -5,14 +8,16 @@ public class InstantiateAvatars : MonoBehaviour
     [SerializeField] private OptionField[] avatarFields;
     [SerializeField] private GameObject avatarPrefab;
     
-    private BuildAvatar _buildAvatar = new();
+    private BuildAvatar _buildAvatar = new ();
 
     public void Instantiate(ParticipantPreferences prefs, AvatarSetupInfos currentTrialAvatarSetupInfos)
     {
+        Debug.Log(prefs);
         for (var i = 0; i < avatarFields.Length; i++)
             avatarFields[i].questionnaireMatch = currentTrialAvatarSetupInfos.MatchChoices[i];
         foreach (var avatarField in avatarFields)
             _buildAvatar.Build(avatarPrefab, avatarField, prefs);
+            
     }
     
 }
